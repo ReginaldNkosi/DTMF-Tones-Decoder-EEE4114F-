191616f376e6f26cb29b9fe06c1edff314cde82c
Kabelo Manake <100381256+MNKKAB001@users.noreply.github.com>
2022-05-20 19:53:27 +0200

    Decoder function
    Compares the frequency to determine which but was pressed

---
+++ decode.m
@@ -0,0 +1,27 @@
+function decode (ol,oh)
+if (ol<= 737.9 && oh <= 1242.8)
+    disp("Key Pressed is 1");
+elseif(ol<= 737.9 && oh<= 1359.3)
+    disp("Key Pressed is 2");
+elseif(ol<= 737.9 && oh<= 1514.7)
+    disp("Key Pressed is 3");
+elseif(ol<= 815.6 && oh<= 1242.8)
+    disp("Key Pressed is 4");
+elseif(ol<= 815.6 && oh<= 1359.3)
+    disp("Key Pressed is 5");
+elseif(ol<= 815.6 && oh<= 1514.7)
+    disp("Key Pressed is 6");
+elseif(ol<= 893.3 && oh<= 1242.8)
+    disp("Key Pressed is 7");
+elseif(ol<= 893.3 && oh<= 1359.3)
+    disp("Key Pressed is 8");
+elseif(ol<= 893.3 && oh<= 1514.7)
+    disp("Key Pressed is 9");
+elseif(ol<= 970.9 && oh<= 1242.8)
+    disp("Key Pressed is *")
+elseif(ol<= 970.9 && oh<= 1359.3)    
+    disp("Key Pressed is 0");
+elseif(ol<= 970.9 && oh<= 1514.7)
+    disp("Key Pressed is #");
+end
+end
